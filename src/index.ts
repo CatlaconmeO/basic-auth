41b53a841eb406da4d27b68b7cc944b11616ddaa
CatlaconmeO <pducdung.it@gmail.com>
2025-05-27 16:18:02 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Elysia } from 'elysia'
 import { auth } from './routes/auth'
 import { swagger } from '@elysiajs/swagger'
 
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
 const app = new Elysia()
   .use(auth)
   .use(swagger())
 
-app.listen(3000)
+app.listen(port)
 
-console.log("🦊 Elysia is running at http://localhost:3000")
+console.log("🦊 Elysia is running at http://localhost:${port}")
