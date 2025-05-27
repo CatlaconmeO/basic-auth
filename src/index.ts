9abc22e1e41722846e1280dfbad357feb9fe81a2
CatlaconmeO <pducdung.it@gmail.com>
2025-05-27 17:30:50 +0700

    update rateLimiter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Elysia } from 'elysia'
 import { auth } from './routes/auth'
 import { swagger } from '@elysiajs/swagger'
+import { rateLimiter } from './middlewares/rateLimiter'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = new Elysia()
+  .onBeforeHandle(rateLimiter)
   .use(auth)
   .use(swagger())
 
---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,33 @@
+import { redisClient } from '../utils/redisClient';
+
+const WINDOW_SIZE_IN_SECONDS = 60; // 1 phút
+const MAX_REQUESTS = 5;
+
+export const rateLimiter = async (context: any) => {
+    const ip = context.request.headers['x-forwarded-for'] 
+    || context.request.headers['x-real-ip'] 
+    || context.request.ip 
+    || 'unknown';
+
+    if (ip == 'unknown') {
+        return { status: 400, message: 'Not Found' };
+    }
+
+    // Cài đặt khoá trong Redis cho từng IP
+    const key = `rate_limit:${ip}`;
+
+    // Kết nối redis nếu chưa kết nối
+    if (!redisClient.isOpen) await redisClient.connect();
+
+    // Lấy key để đếm số request
+    let reqCount = await redisClient.get(key);
+
+    // Xử lý số lượng request
+    if (reqCount === null) {
+        await redisClient.set(key, '1', { EX: WINDOW_SIZE_IN_SECONDS });
+    } else if (parseInt(reqCount) < MAX_REQUESTS) {
+        await redisClient.incr(key);
+    } else {
+        return new Response('Too many requests, please try again later.', { status: 429 });
+    }
+};
